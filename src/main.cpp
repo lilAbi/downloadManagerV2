179e053ed8e78ddab5d48012e978462e8d196640
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-06-29 04:24:05 -0500

    simple sdl window setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* Window{SDL_CreateWindow(
+      "Hello Window", 800, 300, 0
+    )};
+
+    SDL_GetWindowSurface(Window);
+    SDL_UpdateWindowSurface(Window);
+
+    bool IsRunning = true;
+    SDL_Event Event;
+    while (IsRunning) {
+        while (SDL_PollEvent(&Event)) {
+            if (Event.type == SDL_EVENT_QUIT) {
+                IsRunning = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(Window);
+    SDL_Quit();
+
     return 0;
 }
