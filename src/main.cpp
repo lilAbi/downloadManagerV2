a1d61e041667b38170354fb73b2da07f47d6626a
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-06-30 03:12:15 -0500

    new application startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,15 @@
 #include "logger.h"
-#include <SDL3/SDL_main.h>
-#include "window.h"
 #include "application.h"
+
 
 int main() {
     Logger* logger = Logger::get();
 
-    SDL_SetAppMetadata("downloadManagerV2", "0.1.0", "com.downloadManagerV2");
+    if (Application application; application.init()) {
+        application.loop();
+    } else {
+        logger->critical("Application start-up failed");
+    }
 
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
-
-    Window window;
-
-    Application application{window};
-
-    application.loop();
-
-    SDL_Quit();
     return 0;
 }
