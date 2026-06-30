d8f778bc8d67c832e836acae82c94e24aaa4df26
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-06-30 03:10:17 -0500

    expanded window class to setup imgui context with sdl window

--- src/window.cpp
+++ src/window.cpp
@@ -1,23 +1,83 @@
 #include "window.h"
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "../bindings/imgui_impl_opengl3.h"
 
-Window::Window() {
+bool Window::init() {
+    //configure opengl version
+    auto glsl_version = "#version 410";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    //configure window flags
+    constexpr SDL_WindowFlags window_flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+
+    //create sdl window context
+    const float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
     SDL_Window* sdl_window_ptr{
         SDL_CreateWindow(
             "downloadManagerV2",
-            1920/2,
-            1080/2,
-            SDL_WINDOW_RESIZABLE
+            static_cast<int>(1280 * main_scale),
+            static_cast<int>(800 * main_scale),
+            window_flags
         )
     };
+    if (!sdl_window_ptr) {
+        m_logger->info("Error: SDL_CreateWindow(): {}", SDL_GetError());
+        return false;
+    }
 
-    SDL_GetWindowSurface(sdl_window_ptr);
-    SDL_UpdateWindowSurface(sdl_window_ptr);
+    //create opengl graphics context
+    SDL_GLContext gl_context_ptr = SDL_GL_CreateContext(sdl_window_ptr);
+    if (!gl_context_ptr) {
+        m_logger->info("Error: SDL_GL_CreateContext(): {}", SDL_GetError());
+        return false;
+    }
+    SDL_GL_MakeCurrent(sdl_window_ptr, gl_context_ptr);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+    SDL_SetWindowPosition(sdl_window_ptr, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(sdl_window_ptr);
 
-    m_sdl_window_ptr.reset(sdl_window_ptr);
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup scaling
+    ImGuiStyle& style = ImGui::GetStyle();
+    // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+    style.ScaleAllSizes(main_scale);
+    // Set initial font scale. (in docking branch: using io.ConfigDpiScaleFonts=true automatically overrides this for every window depending on the current monitor)
+    style.FontScaleDpi = main_scale;
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForOpenGL(sdl_window_ptr, gl_context_ptr);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    //cache the context ptrs
+    m_gl_context.reset(gl_context_ptr);
+    m_sdl_window.reset(sdl_window_ptr);
+
+    return true;
 }
 
 SDL_Window* Window::get_sdl_window() const {
-    return m_sdl_window_ptr.get();
+    return m_sdl_window.get();
+}
+
+SDL_GLContext Window::get_sdl_gl_context() const {
+    return m_gl_context.get();
 }
 
 SDL_Surface* Window::get_sdl_surface() const {
--- src/window.h
+++ src/window.h
@@ -3,8 +3,9 @@
 #include <memory>
 #include <SDL3/SDL.h>
 
+#include "logger.h"
+
 class Window {
-private:
     struct SDLWindowDeleter {
         void operator()(SDL_Window* sdl_window_ptr) const {
             if (sdl_window_ptr && SDL_WasInit(SDL_INIT_VIDEO)) {
@@ -13,18 +14,30 @@ private:
         }
     };
 
+    struct SDLGlContextDeleter {
+        void operator()(SDL_GLContext sdl_gl_context) const {
+            if (sdl_gl_context) {
+                SDL_GL_DestroyContext(sdl_gl_context);
+            }
+        }
+    };
+
 public:
-    Window();
+    Window() = default;
     ~Window() = default;
 
     Window(const Window&) = delete;
     Window& operator=(const Window&) = delete;
 
 public:
+    bool init();
     [[nodiscard]] inline SDL_Window* get_sdl_window() const;
+    [[nodiscard]] SDL_GLContext get_sdl_gl_context() const;
     [[nodiscard]] SDL_Surface* get_sdl_surface() const;
 
 private:
-    std::unique_ptr<SDL_Window, SDLWindowDeleter> m_sdl_window_ptr;
+    Logger* m_logger{Logger::get()};
+    std::unique_ptr<SDL_Window, SDLWindowDeleter> m_sdl_window;
+    std::unique_ptr<SDL_GLContextState, SDLGlContextDeleter> m_gl_context;
 
 };
