00d7397e2a9f0abf61a3bed1951bc2f0d07a33b3
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-07-03 00:53:09 -0500

    added libcurl init function

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,25 @@
 
 #include "logger.h"
 #include "application.h"
+#include "curl/curl.h"
 
 int main() {
     Logger* logger = Logger::get();
 
-    if (Application application; application.init()) {
-        application.loop();
-    } else {
+    CURLcode result = curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    if(result != CURLE_OK) {
         logger->critical("Application start-up failed");
+    } else {
+        if (Application application; application.init()) {
+            application.loop();
+        } else {
+            logger->critical("Application start-up failed");
+        }
     }
+
+
+    curl_global_cleanup();
 
     return 0;
 }
