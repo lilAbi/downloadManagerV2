fd5b096fc631b5dba617f16f59c6bede9cfa5ded
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-07-16 02:42:18 -0500

    event now holds the payload to pass

--- src/download/downloadControllerEvents.h
+++ src/download/downloadControllerEvents.h
@@ -1,11 +1,13 @@
 #pragma once
 
-#include "event/event.h"
-#include "downloadMetadata.h"
 #include <utility>
 
+#include "event/event.h"
+#include "downloadMetadata.h"
+
 struct DownloadSubmitEvent final : public Event{
-    explicit DownloadSubmitEvent(DownloadSpec download_spec) : m_download_spec(std::move(download_spec)) {}
-    DownloadSpec m_download_spec;
+    explicit DownloadSubmitEvent(std::string source, std::filesystem::path path) : m_source(std::move(source)), m_downloaded_path(std::move(path)) {}
+    std::string             m_source{"no_source_found"};
+    std::filesystem::path   m_downloaded_path{"~/Downloads/"};
     ~DownloadSubmitEvent() override = default;
 };
