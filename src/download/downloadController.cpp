a54acf1e59c2fc33f0f5bc82f3363b8afaade9be
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-07-18 07:31:35 -0500

    added callback for StopDownloaderThread event

--- src/download/downloadController.cpp
+++ src/download/downloadController.cpp
@@ -1,14 +1,19 @@
 #include "downloadController.h"
 
+#include "downloader.h"
 #include "event/eventManager.h"
 
 bool DownloadController::init() {
     EventManager& event_manager = EventManager::get();
+
+    //register callback function to be called
     event_manager.subscribe(this, &DownloadController::on_download_submit_event);
+    event_manager.subscribe(this, &DownloadController::on_stop_downloader_thread_event);
 
-    //star downloader thread
+    //start downloader thread, copy constructed
+    m_downloader_thread = std::jthread(Downloader{&m_command_queue, &m_transfers});
 
-    m_logger->info("DownloadController Initialized");
+    m_logger->trace("DownloadController Initialized");
     return true;
 }
 
@@ -16,7 +21,7 @@ int DownloadController::submit(DownloadSpecification download_spec) {
     //create new uuid for download
     const int new_download_id = DownloadController::m_download_id_counter++;
     //add it to the transfer list
-    if (auto [fst, snd] = m_transfers.try_emplace(new_download_id, std::move(download_spec)); snd) {
+    if (auto [position, isAdded] = m_transfers.try_emplace(new_download_id, std::move(download_spec)); isAdded) {
         m_logger->info("DownloadID added: {}", new_download_id);
     }
     //submit download added command
@@ -42,8 +47,8 @@ DownloadSnapshot DownloadController::get_snapshot(int download_id) {
 
 void DownloadController::on_download_submit_event(std::shared_ptr<DownloadSubmitEvent> event) {
     m_logger->info("DownloadSubmitEvent has been called");
-    m_logger->info("Download Source: {}", event->m_source);
-    m_logger->info("Downloaded Path: {}", event->m_downloaded_path.generic_string());
+    m_logger->info("Incoming Download Source: {}", event->m_source);
+    m_logger->info("Incoming Downloaded Path: {}", event->m_downloaded_path.generic_string());
     //create a new DownloadSpecification obj from incoming event and pass it to be submitted
     this->submit(
         DownloadSpecification{
@@ -53,4 +58,12 @@ void DownloadController::on_download_submit_event(std::shared_ptr<DownloadSubmit
             DownloadState::CREATED
         }
     );
+}
+
+void DownloadController::on_stop_downloader_thread_event(std::shared_ptr<StopDownloaderThreadEvent> event) {
+    m_logger->info("StopDownloaderThreadEvent has been called");
+    m_downloader_thread.request_stop();
+    //block until downloader thread joins
+    m_downloader_thread.join();
+    m_logger->trace("Downloader Thread Joined");
 }
--- src/download/downloadController.h
+++ src/download/downloadController.h
@@ -12,33 +12,40 @@ class DownloadController {
 public:
     DownloadController() = default;
     ~DownloadController() = default;
-
-    //initialize the subsystems
-    bool init();
-
-    int submit(DownloadSpecification download_spec);
-    void pause(int download_id);
-    void resume(int download_id);
-    void cancel(int download_id);
-    //delete?
-    DownloadSnapshot get_snapshot(int download_id);
-
     //delete copy/move assignment/constructors
     DownloadController(const DownloadController&) = delete;
     DownloadController(DownloadController&&) = delete;
     DownloadController& operator=(const DownloadController&) = delete;
     DownloadController& operator=(DownloadController&&) = delete;
 
-private:
-    void on_download_submit_event(std::shared_ptr<DownloadSubmitEvent> event);
+    //initialize the subsystems
+    bool init();
+    //submit a download
+    int submit(DownloadSpecification download_spec);
+    //pause a download
+    void pause(int download_id);
+    //resume a download from being stopped
+    void resume(int download_id);
+    //stop a download
+    void cancel(int download_id);
+    // get a snapshot of a download state - delete?
+    DownloadSnapshot get_snapshot(int download_id);
 
 private:
-    //UUID tracker
+    //callback function to be invoked from ui manager
+    void on_download_submit_event(std::shared_ptr<DownloadSubmitEvent> event);
+
+    void on_stop_downloader_thread_event(std::shared_ptr<StopDownloaderThreadEvent> event);
+
+private:
+    //UUID for unique download operations
     inline static int                           m_download_id_counter = 0;
     //Logger
     Logger*                                     m_logger = &Logger::get();
-    //shared event queue to be passed to thread
-    boost::lockfree::queue< Command >           m_command_queue{0};
+    //Shared Command queue
+    boost::lockfree::queue<Command>             m_command_queue{0};
     //keep track of all transfers that occurred during the lifetime of application
     std::flat_map<int, DownloadSpecification>   m_transfers;
+    //Downloader Thread
+    std::jthread                                m_downloader_thread;
 };
