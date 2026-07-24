f626cfbbaa925ecbbb917711d95967b17e490db6
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-07-23 23:59:03 -0500

    different color output for threads

--- src/core/logger.cpp
+++ src/core/logger.cpp
@@ -7,7 +7,7 @@ Logger& Logger::get() {
 
 Logger::Logger() {
     this->init_loggers();
-    this->info("Logger Initialized");
+    this->trace("Logger Initialized");
 }
 
 Logger::~Logger() {
@@ -19,12 +19,18 @@ void Logger::stop_logging() const {
 }
 
 void Logger::init_loggers() {
+    //configure default sink
     auto default_console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
     default_console_sink->set_level(spdlog::level::trace);
-    default_console_sink->set_pattern("%^[%Y-%m-%d %H:%M:%S.%e] [%n] [thread %t] [%l] %v%$");
+    default_console_sink->set_pattern("\x1b[38;5;88m[%Y-%m-%d %H:%M:%S.%e] [%n] [thread %t]\x1b[0m  %^[%l] %v%$");
+
+    //configure downloader sink
+    auto downloader_console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    downloader_console_sink->set_level(spdlog::level::trace);
+    downloader_console_sink->set_pattern("\x1b[38;5;55m[%Y-%m-%d %H:%M:%S.%e] [%n] [thread %t]\x1b[0m  %^[%l] %v%$");
 
     m_default_logger            = std::make_unique<spdlog::logger>( "Main", spdlog::sinks_init_list{default_console_sink} );
-    m_download_thread_logger    = std::make_shared<spdlog::logger>( "Downloader", spdlog::sinks_init_list{default_console_sink} );
+    m_download_thread_logger    = std::make_shared<spdlog::logger>( "Downloader", spdlog::sinks_init_list{downloader_console_sink} );
 
     m_default_logger->set_level(spdlog::level::trace);
     m_default_logger->flush_on(spdlog::level::err);
