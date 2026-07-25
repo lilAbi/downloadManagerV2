fc657560a3291b88926a007000500d60ab4398cd
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-07-25 17:52:00 -0500

    refactor

--- src/download/downloadControllerEvents.h
+++ src/download/downloadControllerEvents.h
@@ -1,9 +1,8 @@
 #pragma once
 
-#include <utility>
-
 #include "event/event.h"
-#include "downloadMetadata.h"
+#include <utility>
+#include <filesystem>
 
 struct DownloadSubmitEvent final : public Event{
     explicit DownloadSubmitEvent(std::string source, std::filesystem::path path) : m_source(std::move(source)), m_downloaded_path(std::move(path)) {}
