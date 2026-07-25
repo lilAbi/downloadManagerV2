af9d08a2d2468dc58bba750fe4bd25a4b1a0a665
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-07-25 17:51:27 -0500

    reordered definitions and adding supporting classes for "progress update" functionality

--- src/download/downloadMetadata.h
+++ src/download/downloadMetadata.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include "container/threadSafeQueue.h"
+
 #include <filesystem>
 #include <fstream>
 #include <string>
-#include <memory>
+#include <variant>
 #include <curl/curl.h>
 
 enum class DownloadState {
@@ -20,6 +22,30 @@ enum class DownloadState {
     CANCELED
 };
 
+//update an active download with its progress
+struct ProgressUpdate {
+    int m_download_id{-1};
+    int m_bytes_downloaded{};
+    int m_bytes_total{};
+    //double m_bytes_per_second{};
+};
+
+struct StateChangedUpdate {
+    int             m_download_id{-1};
+    DownloadState   m_download_state{};
+};
+
+struct CompletedUpdate {
+    int m_download_id{-1};
+};
+
+struct FailedUpdate {
+    int         m_download_id{-1};
+    std::string m_error;
+};
+
+using DownloadUpdate = std::variant<ProgressUpdate, StateChangedUpdate, CompletedUpdate, FailedUpdate>;
+
 //metadata needed to perform a single download
 struct DownloadSpecification {
     std::string             m_source;
@@ -30,17 +56,20 @@ struct DownloadSpecification {
 
 //metadata representing the state of an active single transfer
 struct DownloadSnapshot {
-    int             m_id;
-    DownloadState   m_download_state;
-    int             m_bytes_download;
-    int             m_bytes_total;
-    double          m_download_speed;
+    int m_id;
+    int m_bytes_total;
+    int m_bytes_downloaded{-1};
+    DownloadState m_download_state;
+    std::string m_source;
+    std::string m_filename;
 };
 
 //metadata needed to manage an active single transfer
 struct ActiveTransfer {
-    int                     m_download_id{-1};
-    CURL*                   m_easy_handle = nullptr;
-    std::fstream            m_file{};
-    DownloadState           m_state{DownloadState::DEFAULT};
+    int             m_download_id{-1};
+    DownloadState   m_state{DownloadState::DEFAULT};
+    CURL*           m_easy_handle = nullptr;
+    std::chrono::steady_clock::time_point last_progress_publish{};
+    ThreadSafeQueue<DownloadUpdate>*      m_progress_update_queue;
+    std::fstream    m_file{};
 };
