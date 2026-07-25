195f8f3eeb08e732f3624c3250cb419d4a648537
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-07-25 17:52:35 -0500

    responding to "progress updates" and added a "view model"

--- src/download/downloadController.cpp
+++ src/download/downloadController.cpp
@@ -1,41 +1,68 @@
 #include "downloadController.h"
-#include "downloader.h"
+#include "downloadViewModelUpdater.h"
 #include "event/eventManager.h"
 
 bool DownloadController::init() {
     EventManager& event_manager = EventManager::get();
-
     //register callback function to be called
     event_manager.subscribe(this, &DownloadController::on_download_submit_event);
     event_manager.subscribe(this, &DownloadController::on_stop_downloader_thread_event);
 
-    //start downloader thread, move constructed
+    //start downloader thread
     m_downloader_thread = std::jthread(&Downloader::operator(), &m_downloader);
 
     m_logger->trace("DownloadController Initialized");
     return true;
 }
 
+void DownloadController::process_download_update_queue() {
+    //passes a lamda func that instructs how to execute the commands in the queue
+    m_downloader_update.drain([this](DownloadUpdate update_variant) {
+            try {
+                //visitor pattern to facilitate how to process incoming update
+                std::visit(
+                    [view_model_updater = DownloadViewModelUpdater{this->m_view_model}] (auto&& update) {
+                        view_model_updater.process_update(update);
+                    },
+                    update_variant
+                );
+            } catch (const std::exception& exception) {
+                m_logger->critical( "Unhandled exception while processing DownloadUpdate: {}", exception.what() );
+            } catch (...) {
+                m_logger->critical("Unknown exception while processing DownloadUpdate");
+            }
+        }
+    );
+}
+
+std::pair<bool, std::unordered_map<int, DownloadSnapshot>*> DownloadController::should_draw_view_model() {
+    return{true, &this->m_view_model};
+}
+
 int DownloadController::submit(DownloadSpecification download_specification) {
     const int new_download_id = DownloadController::m_download_id_counter++;
+    //create a entry in the view model for new active download
+    this->m_view_model[new_download_id] = {
+        .m_id = new_download_id,
+        .m_download_state = DownloadState::CREATED,
+        .m_source = download_specification.m_source,
+        .m_filename = download_specification.m_downloaded_path,
+    };
+    //submit new download to downloader thread
     m_downloader.submit_download_command(new_download_id, std::move(download_specification));
     return new_download_id;
 }
 
 void DownloadController::pause(int download_id) {
-
+    //todo: finish submitting a pause command to downloader
 }
 
 void DownloadController::resume(int download_id) {
-
+    //todo: finish submitting a resume command to downloader
 }
 
 void DownloadController::cancel(int download_id) {
-
-}
-
-DownloadSnapshot DownloadController::get_snapshot(int download_id) {
-    return {};
+    //todo: finish submitting a cancel command to downloader
 }
 
 void DownloadController::on_download_submit_event(std::shared_ptr<DownloadSubmitEvent> event) {
--- src/download/downloadController.h
+++ src/download/downloadController.h
@@ -2,15 +2,14 @@
 
 #include "downloadMetadata.h"
 #include "downloadControllerEvents.h"
-#include "core/logger.h"
-#include <boost/lockfree/queue.hpp>
-
 #include "downloader.h"
+#include "core/logger.h"
 
 class DownloadController {
 public:
     DownloadController() = default;
     ~DownloadController() = default;
+
     //delete copy/move assignment/constructors
     DownloadController(const DownloadController&) = delete;
     DownloadController(DownloadController&&) = delete;
@@ -19,6 +18,12 @@ public:
 
     //initialize the subsystems
     bool init();
+    //void process events in the queue that been queue'd up
+    void process_download_update_queue();
+    //check if we need to draw the view model
+    std::pair<bool, std::unordered_map<int, DownloadSnapshot>*> should_draw_view_model();
+
+private:
     //submit a download
     int submit(DownloadSpecification download_specification);
     //pause a download
@@ -27,12 +32,9 @@ public:
     void resume(int download_id);
     //stop a download
     void cancel(int download_id);
-    // get a snapshot of a download state - delete?
-    DownloadSnapshot get_snapshot(int download_id);
-
-private:
-    //callback function to be invoked from ui manager
+    //callback responding to on "DownloadSubmit" event
     void on_download_submit_event(std::shared_ptr<DownloadSubmitEvent> event);
+    //callback responding to on "StopDownloaderThread" event
     void on_stop_downloader_thread_event(std::shared_ptr<StopDownloaderThreadEvent> event);
 
 private:
@@ -42,6 +44,10 @@ private:
     Logger*                                     m_logger = &Logger::get();
     //hold downloader thread
     std::jthread                                m_downloader_thread;
+    //view model to represent a download and its state
+    std::unordered_map<int, DownloadSnapshot>   m_view_model;
+    //queue to manage events related to an active transfer for downloading
+    ThreadSafeQueue<DownloadUpdate>             m_downloader_update;
     //functor used to manage the "downloader" aspect
-    Downloader                                  m_downloader;
+    Downloader                                  m_downloader{m_downloader_update};
 };
